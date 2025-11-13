9ab2b9577cac2e01e7ac0aeaf722206c4c23877d
Emmanuel Diba De Palmes <ediba-de@f6r6s12.clusters.42paris.fr>
2025-11-13 15:23:42 +0100

    save your tears

--- ft_memset.c
+++ ft_memset.c
@@ -1,11 +1,26 @@
-void	*memset(void *dest, int value, unsigned int size)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ediba-de <ediba-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 14:39:33 by ediba-de          #+#    #+#             */
+/*   Updated: 2025/11/13 15:20:27 by ediba-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *dest, int value, size_t size)
 {
 	int	index;
+	char *dest_ = (char *)dest;
 
 	index = 0;
 	while (index < size)
 	{
-		dest[index] = value;
+		dest_[index] = value;
 		index++;
 	}
 }
