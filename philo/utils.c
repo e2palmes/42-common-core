515c94c286530004ba64c89e6e705ca6f081067e
e2palmes <e2palmes@gmail.com>
2026-08-20 13:37:00 +0200

    fixed number of times param conversion

--- philo/utils.c
+++ philo/utils.c
@@ -5,13 +5,14 @@ t_dinner    *init_dinner(char **argv)
     t_dinner *dinner;
 
     dinner = malloc(sizeof(t_dinner));
-    dinner->number_of_philosophers = argv[1];
-    dinner->time_to_die = argv[2];
-    dinner->time_to_eat = argv[3];
-    dinner->time_to_sleep = argv[4];
-    if ()
-    {}
-    dinner->number_of_times = argv[5] || NULL;
+    dinner->number_of_philosophers = ft_atoi(argv[1]);
+    dinner->time_to_die = ft_atoi(argv[2]);
+    dinner->time_to_eat = ft_atoi(argv[3]);
+    dinner->time_to_sleep = ft_atoi(argv[4]);
+    if (argv[5])
+		dinner->number_of_times = ft_atoi(argv[5]);
+	else
+		dinner->number_of_times = -1;
 	dinner->stop_simulation = FALSE;
 	dinner->start_time = gettimeofday();
 	pthread_mutex_init(&(dinner->stop_mutex), NULL);
